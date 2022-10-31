23662e48d5e4ef08d640f8ff013d9078670030c4
nuskacg <rodelugaliza@gmail.com>
2022-10-31 20:13:01 -0300

    Curso-de-Integracion-de-PHP-con-HTML

---
+++ Como-se-renderiza-una-pagina-web-en-mi-computadora.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+
+https://static.platzi.com/media/public/uploads/2-slides_59473948-d6e0-4686-9153-31abf95d9cf9.pdf
+
+
+----------------------------------------------------------------
+Lecturas recomendadas
+
+Curso Básico de PHP: Instalación, Fundamentos y Operadores - Platzi
+
+https://platzi.com/cursos/php/
+
+
+Curso Básico de PHP: Arreglos, Funciones y Estructuras de Control - Platzi
+
+https://platzi.com/cursos/php-arreglos-funciones/
+
+
+Curso Práctico de PHP - Platzi
+
+https://platzi.com/cursos/php-practico/
+
+
+Cómo interactúa una página web con tu backend - Platzi
+
+https://platzi.com/clases/2646-php/44431-como-interactua-una-pagina-web-con-tu-backend/
+
+
+Tipos de aplicaciones según su router: SPAs vs. SSR - Platzi
+
+https://platzi.com/clases/2239-frameworks-javascript/36181-tipos-de-aplicaciones-segun-su-router-spas-vs-ssr/
+
+*/
+
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Curso-de-Integracion-de-PHP-con-HTML
+### Curso de Integración de PHP con HTML
+
+![PHP](https://static.platzi.com/cdn-cgi/image/width=1024,quality=50,format=auto/media/achievements/piezas-integracion-php-html_badge-e017cef4-d9a5-47bf-814b-68eea412083e.png)
+
+✨- [Curso-de-Integracion-de-PHP-con-HTML](https://platzi.com/cursos/php-html/)
+
+
+
